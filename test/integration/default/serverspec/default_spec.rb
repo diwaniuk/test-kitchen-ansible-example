b42553a81011f215bf80b6f49e288ae20bc196c0
Daniel Iwaniuk <d.iwaniuk@hawatel.com>
2016-11-04 10:57:47 +0100

    Add example local role

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -8,3 +8,19 @@ describe file('/etc/localtime') do
   it { should be_linked_to '/usr/share/zoneinfo/Europe/Warsaw' }
 end
 
+describe file('/etc/ntp.conf') do
+  it { should be_file }
+end
+
+describe file('/etc/ntp.conf') do
+  it { should contain '1.ubuntu.pool.ntp.org' }
+  it { should contain '2.ubuntu.pool.ntp.org' }
+end
+
+describe file('/etc/motd'), :if => os[:family] == 'ubuntu' do
+  it { should contain 'Debian' }
+end
+
+describe file('/etc/motd'), :if => os[:family] == 'redhat' do
+  it { should contain 'RedHat' }
+end
--- test/integration/webservers/serverspec/apache_spec.rb
+++ test/integration/webservers/serverspec/apache_spec.rb
@@ -18,11 +18,6 @@ describe service('apache2'), :if => os[:family] == 'ubuntu' do
   it { should be_running }
 end
 
-describe service('org.apache.httpd'), :if => os[:family] == 'darwin' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
 describe port(80) do
   it { should be_listening }
 end
